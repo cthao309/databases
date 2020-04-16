18ba544a7421a18c76434713efafb84304e8fc73
Charlie Thao <cthao309@gmail.com>
2020-04-15 23:38:45 -0700

    created the schema for the database

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,24 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int NOT NULL AUTO_INCREMENT,
+  userid int NOT NULL,
+  text varchar(100) NOT NULL,
+  roomname varchar(20),
+  PRIMARY KEY(ID)
 );
 
 /* Create other tables and define schemas for them here! */
 
-
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  username varchar(25) NOT NULL,
+  PRIMARY KEY(ID)
+)
 
 
 /*  Execute this file from the command line by typing:
